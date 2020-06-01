52de0e39849f08cb6aa502e3dc7b4b669b486aa1
daniel evelyne moshayof <danielevelyne.moshayof@live.biu.ac.il>
2020-06-01 10:23:24 +0300

    calc dists

--- SOM.py
+++ SOM.py
@@ -7,8 +7,9 @@ path = 'Digits_Ex3.txt'
 NN_SIZE = 6
 class Neuron():
 
-    def __init__(self,K,N):
-        self.vector  = np.zeros(N)
+    def __init__(self,N):
+        K = np.random.randint(0,100)
+        self.vector = np.zeros(N)
         self.vector[:K] = 1
         np.random.shuffle(self.vector)
         self.color = None
@@ -18,8 +19,13 @@ class Neuron():
 
 class Board():
 
-    def __init__(self,K,N):
-        self.net_work = [[Neuron(K,N)]*NN_SIZE]*NN_SIZE
+    def __init__(self,N):
+        self.net_work = [[]*NN_SIZE]*NN_SIZE
+        for i in range(0,NN_SIZE):
+            self.net_work[i].append( Neuron(N))
+
+
+
 
     def calc_dist(self,new_vec,neuron):
         sum = 0
@@ -32,9 +38,14 @@ class Board():
         dist = {}
         for sample in data:
             sample = sample.replace('\n','')
-            for row in self.net_work:
-                for i,neuron in enumerate(row):
-                    dist[neuron]=(self.calc_dist(sample,neuron))
+            for i,row in enumerate(self.net_work):
+                for j,neuron in enumerate(row):
+                    dist[(i,j)]=(self.calc_dist(sample,neuron))
+            min_item = min(dist.items(), key=lambda x: x[1])
+            n_row = min_item[0][0]
+            n_col = min_item[0][1]
+            self.net_work[n_row][n_col].update(sample)
+
 
 
 def main():
@@ -49,8 +60,8 @@ def main():
                 matrix[i][j] =num[i][j]
         im = plt.imshow(matrix)
         #plt.pause(0.01)
-    board = Board(36,100)
-    board.train(all_num[0].replace('\n',''))
+    board = Board(N=100)
+    board.train(all_num)
 
 
 if __name__ == '__main__':
