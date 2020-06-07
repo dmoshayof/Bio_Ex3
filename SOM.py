29745b0de4c5b8b2b6ec0caa882c1150bf77fc95
daniel evelyne moshayof <danielevelyne.moshayof@live.biu.ac.il>
2020-06-07 17:37:18 +0300

    Almost done

--- SOM.py
+++ SOM.py
@@ -1,10 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets import load_digits
-
+from sklearn.metrics.pairwise import euclidean_distances
+import math
+import random
 MATRIX_SIZE = 10
 path = 'Digits_Ex3.txt'
 NN_SIZE = 6
+EPOCHS = 72
+ALPHA = 0.9
+SIGMA = 0.5
 class Neuron():
 
     def __init__(self,N):
@@ -14,37 +19,82 @@ class Neuron():
         np.random.shuffle(self.vector)
         self.color = None
 
+    def set_colors(self,color):
+        self.color = color
+
     def update(self,vector):
         return
 
 class Board():
 
     def __init__(self,N):
-        self.net_work = [[]*NN_SIZE]*NN_SIZE
-        for i in range(0,NN_SIZE):
-            self.net_work[i].append( Neuron(N))
+        self.net_work =[[0]*NN_SIZE for i in range(NN_SIZE)]
+        for i in range(NN_SIZE):
+            random.seed(random.randint(0,100))
+            for j in range(NN_SIZE):
+                self.net_work[i][j] = np.random.randint(0,2,[MATRIX_SIZE,MATRIX_SIZE])
+                np.random.shuffle(self.net_work[i][j])
 
-
-
-
-    def calc_dist(self,new_vec,neuron):
+    def calc_dist(self,i_matrix,cell):
         sum = 0
-        for i in range(0,np.size(neuron.vector)):
-            sum += pow(int(new_vec[i])-neuron.vector[i],2)
+        for i in range(MATRIX_SIZE):
+            for j in range(MATRIX_SIZE):
+                sum += pow(int(i_matrix[i][j])-cell[i][j],2)
         total = np.sqrt(sum)
         return total
 
+    def update(self,i,j, i_matrix,alpha,sigma):
+        for x in range(NN_SIZE):
+            for y in range(NN_SIZE):
+                new_row = []
+                k =0
+                for row in self.net_work[x][y]:
+                    dist = math.sqrt((i-x)**2+(j-y)**2)
+                    h = alpha *(np.exp(-dist/(2*(sigma**2))))
+                    new_row.append(row+(h*(i_matrix[k]-row)))
+                    k +=1
+                self.net_work[x][y] = new_row
+        return
+    def convert_to_matrix(self,sample):
+        matrix = np.zeros([10, 10])
+        num = sample.split('\n')
+        for i in range(0, 10):
+            for j in range(0, 10):
+                matrix[i][j] = num[i][j]
+        return matrix
+    def show_image(self):
+        # subplot(r,c) provide the no. of rows and columns
+        f, axarr = plt.subplots(6,6)
+        for i in range(NN_SIZE):
+            for j in range(NN_SIZE):
+             axarr[i][j].imshow(self.net_work[j][i])
+        [axi.set_axis_off() for axi in axarr.ravel()]
+        plt.waitforbuttonpress()
+        plt.close()
+
     def train(self,data):
         dist = {}
-        for sample in data:
-            sample = sample.replace('\n','')
-            for i,row in enumerate(self.net_work):
-                for j,neuron in enumerate(row):
-                    dist[(i,j)]=(self.calc_dist(sample,neuron))
-            min_item = min(dist.items(), key=lambda x: x[1])
-            n_row = min_item[0][0]
-            n_col = min_item[0][1]
-            self.net_work[n_row][n_col].update(sample)
+        t_alpha = ALPHA
+        t_sigma = SIGMA
+
+
+        for e in range(EPOCHS):
+            if e == 20:
+                np.random.shuffle(data)
+            for sample in data:
+                i_matrix = self.convert_to_matrix(sample)
+                for i in range(NN_SIZE):
+                    for j in range(NN_SIZE):
+                        dist[(i,j)]=(self.calc_dist(i_matrix,self.net_work[i][j]))
+                min_item = min(dist.items(), key=lambda x: x[1])
+                n_row = min_item[0][0]
+                n_col = min_item[0][1]
+                self.update(n_row,n_col,i_matrix,t_alpha,t_sigma)
+            t_alpha = ALPHA*(t_alpha/ALPHA)**(e/EPOCHS)
+            t_sigma = SIGMA*(t_sigma/SIGMA)**(e/EPOCHS)
+
+        self.show_image()
+
 
 
 
@@ -52,14 +102,6 @@ def main():
     with open(path) as data:
         all_num = data.read().split('\n\n')
 
-    matrix = np.zeros([10,10])
-    for num in all_num:
-        num = num.split('\n')
-        for i in range(0,10):
-            for j in range(0,10):
-                matrix[i][j] =num[i][j]
-        im = plt.imshow(matrix)
-        #plt.pause(0.01)
     board = Board(N=100)
     board.train(all_num)
 
