4a6759a1dec04c7c77aa4024170c939d943eba6d
Daniel Moshayof <43912232+dmoshayof@users.noreply.github.com>
2020-06-10 17:29:24 +0300

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import random
 MATRIX_SIZE = 10
 path = 'Digits_Ex3.txt'
 NN_SIZE = 6
-EPOCHS = 1
+EPOCHS = 5
 ALPHA = 0.9
 SIGMA = 0.4
 
@@ -83,21 +83,14 @@ class SOM():
 
     def show_image(self,data):
         # subplot(r,c) provide the no. of rows and columns
-        f, axarr = plt.subplots(16, 16)
+        f, axarr = plt.subplots(6, 6)
         for i in range(NN_SIZE):
             for j in range(NN_SIZE):
-                axarr[i][j].imshow(self.net_work[j][i])
-        count = 0
-
-        for i in range(MATRIX_SIZE):
-            for j in range(MATRIX_SIZE):
-                axarr[i+6][j+6].imshow(data[count][0])
-                axarr[i+6][j+6].set_title(data[count][1])
-                count+=1
+                axarr[i][j].imshow(self.net_work[NN_SIZE-i-1][j])
 
         [axi.set_axis_off() for axi in axarr.ravel()]
-        plt.waitforbuttonpress()
-        plt.close()
+
+        f.suptitle("Final Network")
 
     def find_nearest_neuron(self, i_matrix):
         """
@@ -116,82 +109,91 @@ class SOM():
 
     def train(self, all_matrix):
         save = []
-
+        x = np.zeros(100)
+        y = np.zeros(100)
         t_alpha = ALPHA
         t_sigma = SIGMA
         for e in range(EPOCHS):
-            #np.random.shuffle(data)  #For data shuffling state
+            np.random.shuffle(all_matrix)  #For data shuffling state
             avg_quantization_error = 0
-            for i_matrix in all_matrix:
+            for i,i_matrix in enumerate(all_matrix):
                 #sample = random.choice(data) #for random data state
                 n_row, n_col = self.find_nearest_neuron(i_matrix)
                 self.update(n_row, n_col, i_matrix, t_alpha, t_sigma)
                 avg_quantization_error += self.calc_dist(i_matrix, self.net_work[n_row][n_col])
                 if e == EPOCHS -1:
                     save.append((i_matrix,(n_row,n_col)))
+                    if n_row in y:
+                        n_row +=np.random.uniform(0,0.5)
+                    y[i] = n_row
+                    x[i] = n_col
             avg_quantization_error /= MATRIX_SIZE*MATRIX_SIZE
             print("Epoch: {} , quantization error: {}".format(e, avg_quantization_error))
             t_alpha = ALPHA * (t_alpha / ALPHA) ** (e / EPOCHS)
             t_sigma = SIGMA * (t_sigma / SIGMA) ** (e / EPOCHS)
-        self.show_image(save)
-        return save
 
-# Generate data x, y for scatter and an array of images.
-x = np.arange(6)
-y = np.arange(6)
+        return save,x,y
 
-with open(path) as data:
-    all_num = data.read().split('\n\n')
-som = SOM()
-arr = som.convert_to_matrix(all_num)
-#som.train(arr)
-# create figure and plot scatter
-fig = plt.figure()
-ax = fig.add_subplot(111)
-line, = ax.plot(x,y, ls="", marker="o")
+def generate_figures(arr,x,y):
 
-# create the annotations box
-im = OffsetImage(arr[0,:,:], zoom=5)
-xybox=(50., 50.)
-ab = AnnotationBbox(im, (0,0), xybox=xybox, xycoords='data',
-        boxcoords="offset points",  pad=0.3,  arrowprops=dict(arrowstyle="->"))
-# add it to the axes and make it invisible
-ax.add_artist(ab)
-ab.set_visible(False)
+    # create figure and plot scatter
+    fig = plt.figure(2)
+    fig.suptitle("Samples association for each neuron")
+    ax = fig.add_subplot(111)
+    line, = ax.plot(x,y, ls="", marker="o")
+    cell = 0
+    for i in range(NN_SIZE):
+        for j in range(NN_SIZE):
+            ax.plot(i,j,marker= "D",ls="",color= "red")
+            plt.text(j,NN_SIZE-i-1,cell)
+            cell+=1
+    # create the annotations box
+    im = OffsetImage(arr[0,:,:], zoom=5)
+    xybox=(50., 50.)
+    ab = AnnotationBbox(im, (0,0), xybox=xybox, xycoords='data',
+            boxcoords="offset points",  pad=0.3,  arrowprops=dict(arrowstyle="->"))
+    # add it to the axes and make it invisible
+    ax.add_artist(ab)
+    ab.set_visible(False)
 
-def hover(event):
-    # if the mouse is over the scatter points
-    if line.contains(event)[0]:
-        # find out the index within the array from the event
-        ind, = line.contains(event)[1]["ind"]
-        # get the figure size
-        w,h = fig.get_size_inches()*fig.dpi
-        ws = (event.x > w/2.)*-1 + (event.x <= w/2.)
-        hs = (event.y > h/2.)*-1 + (event.y <= h/2.)
-        # if event occurs in the top or right quadrant of the figure,
-        # change the annotation box position relative to mouse.
-        ab.xybox = (xybox[0]*ws, xybox[1]*hs)
-        # make annotation box visible
-        ab.set_visible(True)
-        # place it at the position of the hovered scatter point
-        ab.xy =(x[ind], y[ind])
-        # set the image corresponding to that point
-        im.set_data(arr[ind,:,:])
-    else:
-        #if the mouse is not over a scatter point
-        ab.set_visible(False)
-    fig.canvas.draw_idle()
+    def hover(event):
+        # if the mouse is over the scatter points
+        if line.contains(event)[0]:
+            # find out the index within the array from the event
+            ind = line.contains(event)[1]["ind"]
+            ind = np.array(ind[0])
+            # get the figure size
+            w,h = fig.get_size_inches()*fig.dpi
+            ws = (event.x > w/2.)*-1 + (event.x <= w/2.)
+            hs = (event.y > h/2.)*-1 + (event.y <= h/2.)
+            # if event occurs in the top or right quadrant of the figure,
+            # change the annotation box position relative to mouse.
+            ab.xybox = (xybox[0]*ws, xybox[1]*hs)
+            # make annotation box visible
+            ab.set_visible(True)
+            # place it at the position of the hovered scatter point
+            ab.xy =(x[ind], y[ind])
+            # set the image corresponding to that point
+            im.set_data(arr[ind,:,:])
+        else:
+            #if the mouse is not over a scatter point
+            ab.set_visible(False)
+        fig.canvas.draw_idle()
 
-# add callback for mouse moves
-fig.canvas.mpl_connect('motion_notify_event', hover)
-plt.show()
+    # add callback for mouse moves
+    fig.canvas.mpl_connect('motion_notify_event', hover)
+    plt.grid(True)
+    plt.show()
+
+
 def main():
     with open(path) as data:
         all_num = data.read().split('\n\n')
-
     som = SOM()
-    som.train(all_num)
-
+    arr = som.convert_to_matrix(all_num)
+    save, x, y = som.train(arr)
+    som.show_image(save)
+    generate_figures(arr,x,y)
 
 if __name__ == '__main__':
     main()
